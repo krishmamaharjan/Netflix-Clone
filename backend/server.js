8bf60100fdd7dec7044021d42c0302b4eaa270ce
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-16 22:43:52 +0545

    middleware protectRoute added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 
 import authRoutes from "./routes/auth.routes.js";
 import movieRoutes from "./routes/movie.routes.js";
@@ -6,6 +7,7 @@ import tvRoutes from "./routes/tv.routes.js";
 
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
+import { protectRoute } from './middleware/protectRoute.js';
 // import dotenv from "dotenv";
 
 // dotenv.config();
@@ -14,10 +16,13 @@ const app = express();
 const PORT = ENV_VARS.PORT;
 
 // console.log("MONGO_URI: ", process.env.MONGO_URI);
+
 app.use(express.json());    //will allow us to parse req.body
+app.use(cookieParser());
+
 app.use("/api/v1/auth",authRoutes);
-app.use("/api/v1/movie",movieRoutes);
-app.use("/api/v1/tv",tvRoutes);
+app.use("/api/v1/movie",protectRoute,movieRoutes);
+app.use("/api/v1/tv",protectRoute,tvRoutes);
 
 
 app.listen(PORT, () =>{ 
