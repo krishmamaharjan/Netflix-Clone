b93c9534715974bd8cb487dcb2414009635e12e7
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-16 21:50:40 +0545

    movie routes and controller added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
+
 import authRoutes from "./routes/auth.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
+
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 // import dotenv from "dotenv";
@@ -7,13 +10,18 @@ import { connectDB } from './config/db.js';
 // dotenv.config();
 const app = express();
 
+const PORT = ENV_VARS.PORT;
+
 // console.log("MONGO_URI: ", process.env.MONGO_URI);
 app.use(express.json());    //will allow us to parse req.body
-app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/movie",movieRoutes);
 
-const PORT = ENV_VARS.PORT
 
 app.listen(PORT, () =>{ 
     console.log("Server started at http://localhost:"+PORT);
     connectDB();
 });
+
+
+
