69a7f4c6b64a414ce6271d46376acb9ced9e9ecd
krishmamaharjan <krishmamaharjan73@gmail.com>
2025-01-16 21:50:12 +0545

    movie database added

---
+++ backend/services/tmdb.service.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { ENV_VARS } from '../config/envVars.js';
+
+
+  
+//   fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options)
+//     .then(res => res.json())
+//     .then(res => console.log(res))
+//     .catch(err => console.error(err));
+
+export const fetchFromTMDB = async (url) => {
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer ' + ENV_VARS.TMDB_API_KEY
+        }
+      };
+      const response = await axios.get(url, options)
+
+      if(response.status !== 200)
+      {
+        throw new Error("Failed to fetch data from TMDB" + response.statusText);
+      }
+      return response.data;
+}
